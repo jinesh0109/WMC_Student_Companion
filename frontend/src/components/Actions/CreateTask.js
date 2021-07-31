6240c4c9139ad508119c08ebef3f1f2305b0143f
Poojan Gandhi <poojanngandhi@gmail.com>
2021-07-31 14:53:49 +0530

    CreateTaskBG

--- frontend/src/components/Actions/CreateTask.js
+++ frontend/src/components/Actions/CreateTask.js
@@ -39,9 +39,10 @@ const CreateTask=()=>{
             console.log(error.message);console.log(error.response);
             
         });
-    });
+    })
+    // background: 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%';
     return (
-        <div style={{background: 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%'
+        <div style={{backgroundColor:''
         }}>
             
             <br/><br/>
